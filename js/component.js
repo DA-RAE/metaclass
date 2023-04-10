8a6441ae9d7f6f9151c8575ceb48faae1a7d8201
Taeyeong Sun <styd4957@gmail.com>
2023-04-10 21:26:07 +0900

    20230410

--- js/component.js
+++ js/component.js
@@ -1,58 +1,16 @@
-export const app = document.getElementById('app');
+export default class Component {
+    app = document.getElementById('app');
 
-export class Etc {
     br() {
         const br = document.createElement('br');
         app.appendChild(br)
     }
-}
 
-export class Button {
-    main() {
+    urlButton(id, text, css) {
         const button = document.createElement('button');
-        button.textContent = 'META CLASS';
-        button.classList.add('indexButton');
-        button.id = '/';
+        button.id = id;
+        button.textContent = text;
+        button.classList.add(css);
         app.appendChild(button);
     }
-
-    word() {
-        const button = document.createElement('button');
-        button.textContent = '단어학습';
-        button.classList.add('tableButton');
-        button.id = '/word';
-        app.appendChild(button);
-    }
-
-    speak() {
-        const button = document.createElement('button');
-        button.textContent = '회화연습';
-        button.classList.add('tableButton');
-        button.id = '/speak';
-        app.appendChild(button);
-    }
-
-    member() {
-        const button = document.createElement('button');
-        button.textContent = '회원관리';
-        button.classList.add('tableButton');
-        button.id = '/member';
-        app.appendChild(button);
-    }
-
-    grade() {
-        const button = document.createElement('button');
-        button.textContent = '성적관리';
-        button.classList.add('tableButton');
-        button.id = '/grade';
-        app.appendChild(button);
-    }
-}
-
-export class Table {
-
-}
-
-export class Input {
-
 }
--- js/initialize.js
+++ js/initialize.js
@@ -1,16 +1,16 @@
-// 이벤트 핸들러를 등록시켜 이벤트 발생시 컴포넌트를 렌더링하는 함수를 실행
-window.addEventListener('load', renderComponent);
-window.addEventListener('popstate', renderComponent);
+// 이벤트 핸들러를 등록시켜 이벤트 발생시 화면을 렌더링하는 함수를 실행
+window.addEventListener('load', render);
+window.addEventListener('popstate', render);
 document.addEventListener('click', (event) => {
     if (event.target.tagName === 'BUTTON' && event.target.id[0] === '/') {
         event.preventDefault();
         history.pushState(null, null, event.target.id);
-        renderComponent();
+        render();
     }
 });
 
 // 현재 URL에 따라 각 URL의 초기 화면을 렌더링
-function renderComponent() {
+function render() {
     const route = {
         '/index': initializeMain,
         '/': initializeMain,
@@ -19,26 +19,24 @@ function renderComponent() {
     route[window.location.pathname]();
 }
 
-import * as component from "./component.js";
+import Component from "./component.js";
 
 function initializeMain() {
     document.title = 'MetaClass 관리자 페이지';
+    const component = new Component();
     component.app.innerHTML = '';
-    const btn = new component.Button;
-    const etc = new component.Etc;
-    btn.main();
-    etc.br();
-    btn.word();
-    btn.speak();
-    btn.member();
-    btn.grade();
+    component.urlButton('/', 'META CLASS', 'mainButton');
+    component.br();
+    component.urlButton('/word', '단어학습', 'pageButton');
+    component.urlButton('/speak', '회화연습', 'pageButton');
+    component.urlButton('/member', '회원관리', 'pageButton');
+    component.urlButton('/grade', '성적관리', 'pageButton');
 }
 
 function initializeWord() {
     document.title = '단어게임';
+    const component = new Component();
     component.app.innerHTML = '';
-    const btn = new component.Button;
-    const etc = new component.Etc;
-    btn.main();
-    etc.br();
+    component.urlButton('/', 'META CLASS', 'mainButton');
+    component.br();
 }
